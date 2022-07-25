e5ad584774993e63ef2e5aef67f6a82ce9b1bd54
vinccodes <vinccodes@gmail.com>
2022-07-24 23:37:46 -0400

    style: styled addNoteForm

--- solo-projects/tier3/journal-app/src/components/AddNoteForm.js
+++ solo-projects/tier3/journal-app/src/components/AddNoteForm.js
@@ -2,18 +2,18 @@ import React from 'react'
 
 const AddNoteForm = (props) =>{
     return(
-        <div className="container">
-            <form>
-                <div className="">
-                    <label htmlFor="title">Title:</label>
-                    <input name="title" type="text"></input>
+        <div className="addNoteForm__container">
+            <form className="addNoteForm">
+                <div className="form__group">
+                    <label className="form__label" htmlFor="title">Title:</label>
+                    <input className="form__field" name="title" type="text"></input>
                 </div>
-                <div className="">
-                    <label htmlFor="body">Body:</label>
-                    <textarea name="body"></textarea>
+                <div className="form__group">
+                    <label className="form__label" htmlFor="body">Body:</label>
+                    <textarea className="form__field"name="body"></textarea>
                 </div>
-                <p>Use the form above to create a post. Make sure you fill the required title and body fields and then press submit.</p>
-                <button className="" type="submit">Submit</button>
+                <p className="addNoteForm__description">Use the form above to create a post. Make sure you fill the required title and body fields and then press submit.</p>
+                <button className="form__btn btn__submit" type="submit">Submit</button>
             </form>
         </div>
        
