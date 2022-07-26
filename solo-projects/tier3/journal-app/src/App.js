6097dd898ce348eab9e7bee32b45f4334ad840a7
vinccodes <vinccodes@gmail.com>
2022-07-25 23:46:30 -0400

    Added onChange handlers to AddNoteForm, added LoginForm to App.js

--- solo-projects/tier3/journal-app/src/App.js
+++ solo-projects/tier3/journal-app/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import './styles.css';
@@ -6,10 +7,41 @@ import {Navbar, AddNoteForm, Note} from './components'
 
 function App() {
 
+  const [username, setUsername] = React.useState("")
+  const [password, setPassword] = React.useState("")
+
+  
+  const handleLoginChange = (event)=>{
+    if (event.target.username === "username") {
+      setUsername(event.target.value)
+    }
+    else if (event.target.password === "password") {
+      setPassword(event.target.value)
+    }
+  }
+
+  console.log('username', username)
+  console.log('password', password)
+
+  // Login handler
+  const handleLogin = (event)=>{
+
+    event.preventDefault()
+    // get the username and password from the form
+    const username = event.target.username.value
+    const password = event.target.password.value
+
+    console.log('handleLogin username:', username)
+    console.log('handleLogin password:', password)
+  }
+
+
+
+  // Map all Note objects retrieved from the server to JSX Note elements
   const allNotes = data.map(note =>{
     return(
       <Note 
-          key={note.id} 
+          key={note.id}
           title={note.title}
           body={note.body}
         />
@@ -20,8 +52,31 @@ function App() {
       <header>
         <Navbar logo="Digital Journal" subtitle=" | Create a Note"/>
       </header>
+
+      <div className="container__login">
+        <form className="login__form" method="POST" onSubmit={handleLogin}>
+          <div className="form__group">
+            <label className="form__label" htmlFor="username">Username:</label>
+            <input className="form__field" 
+              type="text"
+              name="username" 
+              value={username}
+              onChange={handleLoginChange}></input>
+          </div>
+          <div className="form__group">
+            <label className="form__label" htmlFor="password">Password:</label>
+            <input className="form__field" 
+              type="password" 
+              name="password"
+              value={password}
+              onChange={handleLoginChange}></input>
+          </div>
+          <button className="form__btn btn__submit" type="submit">Submit</button>
+        </form>
+      </div>
+
       <AddNoteForm/>
-      <div class="container__notes">
+      <div className="container__notes">
         {allNotes}
       </div>
     </div>
--- solo-projects/tier3/journal-app/src/components/AddNoteForm.js
+++ solo-projects/tier3/journal-app/src/components/AddNoteForm.js
@@ -1,16 +1,52 @@
 import React from 'react'
+import noteService from '../services/noteService'
+
 
 const AddNoteForm = (props) =>{
+    const [title, setTitle] = React.useState('')
+    const [body, setBody] = React.useState('')
+
+    // event handlers for onChange for FormFields
+    const handleFormInput = (event) => {
+        // check if event element is title or body
+        if (event.target.name === "title") {
+            setTitle(event.target.value)
+        }
+        else if (event.target.name === "body"){
+            setBody(event.target.value)
+        }
+    }
+
+    // send note to server 
+    const submitNote = (event) =>{
+        event.preventDefault()
+        // get the title and body values from the form
+        const title =  event.target.title.value
+        const body = event.target.body.value
+
+        // send POST request to server
+        
+    }
+
+    console.log('title: ', title)
+    console.log('body: ',body)
     return(
         <div className="addNoteForm__container">
-            <form className="addNoteForm">
+            <form className="addNoteForm" method="POST" onSubmit={submitNote}>
                 <div className="form__group">
                     <label className="form__label" htmlFor="title">Title:</label>
-                    <input className="form__field" name="title" type="text"></input>
+                    <input className="form__field" 
+                        name="title" 
+                        type="text"
+                        onChange={handleFormInput}
+                        value={title}></input>
                 </div>
                 <div className="form__group">
                     <label className="form__label" htmlFor="body">Body:</label>
-                    <textarea className="form__field"name="body"></textarea>
+                    <textarea className="form__field" 
+                    name="body"
+                    onChange={handleFormInput}
+                    value={body}></textarea>
                 </div>
                 <p className="addNoteForm__description">Use the form above to create a post. Make sure you fill the required title and body fields and then press submit.</p>
                 <button className="form__btn btn__submit" type="submit">Submit</button>
