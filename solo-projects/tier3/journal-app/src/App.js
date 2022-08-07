04dcb552e97ba636ba6383fee89415d111031658
vinccodes <vinccodes@gmail.com>
2022-08-07 00:03:05 -0400

    test

--- solo-projects/tier3/journal-app/src/App.js
+++ solo-projects/tier3/journal-app/src/App.js
@@ -39,6 +39,7 @@ function App() {
     try {
       // send request to /auth/login
       const user = await loginService.login({username, password})
+      // TODO
       setCurrentUser(user)
       setUsername('')
       setPassword('')
